875af537291a6fd721e0211cad65eb18a9b8f1cc
Joseph765 <joseph.chauvin102@gmail.com>
2020-05-15 12:41:46 -0500

    Refactored to css modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Styles from './components/Suggested.module.css';
 import App from './components/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('suggested'));
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
             plugins: ['@babel/plugin-proposal-object-rest-spread']
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
       }
     ]
   }
